ce5184bacd18da66208cf017719524c3194377bc
mtejedorg <marco.bibi@gmail.com>
2016-06-28 00:25:13 +0200

    A* pathfinding algorithm

--- src/global_navigation/MyAlgorithm.py
+++ src/global_navigation/MyAlgorithm.py
@@ -1,8 +1,12 @@
 from sensors import sensor
 import numpy as np
+from math import sqrt
 import cv2
+import A_Pathfinding as alg
 
 class MyAlgorithm():
+
+    first = True
 
     def __init__(self, sensor, grid):
         self.sensor = sensor
@@ -20,7 +24,39 @@ class MyAlgorithm():
         dest = self.grid.getDestiny()   
         gridPos = self.grid.getPose()
 
+        dest = (dest[1],dest[0])
+        gridPos = (gridPos[1], gridPos[0])
+
+        if self.first:
+            print mapIm
+
+            for y in range(len(mapIm)):
+                for x in range(len(mapIm[0])):
+                    if mapIm[y][x] > 0:
+                        mapIm[y][x] = 0
+                    else:
+                        mapIm[y][x] = 1
+
+
+            print mapIm
+            self.first = False
+
+
         #TODO
+        nmap = np.array(mapIm)
+        pathinstructions = alg.astar(nmap, dest, gridPos)
+
+        # Fill distance grid
+        pos = len(pathinstructions)
+        for inst in pathinstructions:
+            self.grid.setVal(inst[1], inst[0], pos)
+            pos = pos-1
+
+        # Fill path
+        for inst in pathinstructions:
+            self.grid.setPathVal(inst[1], inst[0], 1)
+
+        self.grid.setPathFinded()
 
         #Represent the Gradient Field in a window using cv2.imshow
         self.grid.showGrid()
